ba160655ac7b3970cc5de39db58c4266a4eb9540
Christian <hacs@zhaw.ch>
2023-11-15 15:24:32 +0100

    updates

--- example.jl
+++ example.jl
@@ -12,4 +12,4 @@ af1 = Airfoil("data/NACA0012.dat")
 afs_int = interpolate_airfoils([af, af1, af1], [0., 0.5, 1.0], Vector(LinRange(0, 1, 10)))
 
 
-save(af1, "data/NACA0012_new.csv")
+# save.(afs_int, [joinpath("data", af.name * ".dat") for af in afs_int])
