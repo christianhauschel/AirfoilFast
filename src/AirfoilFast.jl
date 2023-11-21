13a12b3daa1816a6a2ad34e82ed6faeb7de7bc66
Christian <hacs@zhaw.ch>
2023-11-21 10:58:34 +0100

    bugfix save_dust

--- src/AirfoilFast.jl
+++ src/AirfoilFast.jl
@@ -520,13 +520,24 @@ function save(af::Airfoil, fname)
         CSV.write(fname, df)
     end
 end 
+"""
+    save_dust(af::Airfoil, fname)
 
+Saves the airfoil for the dust solver.
+
+The curve must start at the TE, pass from the lower side of the airfoil, the LE, 
+the upper side and end again at the TE. The first and last point can be not 
+coinciding, to generate an open TE. 
+"""
 function save_dust(af::Airfoil, fname)
+    x = reverse(af.x)
+    y = reverse(af.y)
+    
     f = open(fname, "w")
-    println(f, length(af))
-    for i = 1:length(af)
-        println(f, af.x[i], " ", af.y[i])
-    end
+        println(f, length(af))
+        for i = 1:length(af)
+            println(f, x[i], " ", y[i])
+        end
     close(f)
 end
 
